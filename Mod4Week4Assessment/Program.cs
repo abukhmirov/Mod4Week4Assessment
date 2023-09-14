6d72ac0db2f09d8c2c91816c558a8b35381694ed
abukhmirov <alexbukhmirov@gmail.com>
2023-09-14 10:28:31 -0600

    Final Commit

--- Mod4Week4Assessment/Program.cs
+++ Mod4Week4Assessment/Program.cs
@@ -1,77 +1,123 @@
 ﻿using System;
 using System.Collections.Generic;
 
-public class Student
+
+
+public class Person
 {
     public string Name { get; private set; }
-    public int Grade { get; private set; }
     public DateTime Birthday { get; private set; }
 
-    public Student(string name, int grade, DateTime birthday)
+    public Person(string name, DateTime birthday)
     {
         Name = name;
-        Grade = grade;
         Birthday = birthday;
-    }
-
-    public string GetGreeting()
-    {
-        return $"Hello, I'm {Name}, a student in grade {Grade}!";
     }
 
     public string GetBirthday()
     {
         return $"{Name}'s birthday is on {Birthday:MMMM d, yyyy}";
     }
+
+    public string GetGreeting()
+    {
+        return $"Hello, I'm {Name}!";
+    }
+
 }
 
-public class Teacher
+
+
+public class Student : Person
 {
-    public string Name { get; private set; }
-    public DateTime Birthday { get; private set; }
+   
+    public int Grade { get; private set; }
+    
+
+    public Student(string name, int grade, DateTime birthday) : base(name, birthday)
+    {
+        
+        Grade = grade;
+        
+    }
+
+    public new string GetGreeting()
+    {
+        return $"Hello, I'm {Name}, a student in grade {Grade}!";
+    }
+
+    public new string GetBirthday()
+    {
+        return $"{Name}'s birthday is on {Birthday:MMMM d, yyyy}";
+    }
+
+}
+
+public class Teacher : Person
+{
+    
+   
     public string Subject { get; private set; }
 
-    public Teacher(string name, string subject, DateTime birthday)
+    public Teacher(string name, string subject, DateTime birthday) : base (name, birthday) 
     {
-        Name = name;
-        Birthday = birthday;
+       
+        
         Subject = subject;
     }
 
-    public string GetGreeting()
+    public new string GetGreeting()
     {
         return $"Hello, I'm {Name}, a {Subject} teacher!";
     }
 
-    public string GetBirthday()
+    public new string GetBirthday()
     {
         return $"{Name}'s birthday is on {Birthday:MMMM d, yyyy}";
     }
 }
 
 // Create a school class that uses dependency injection by taking in a list of people.
-public class School { }
+
+
+public class School 
+{
+public List<Person> people;
+
+    public School(List<Person> people)
+    {
+        this.people = people;
+    }
+
+    public void ListBirthdays()
+    {
+        foreach (Person person in people) 
+        {
+            Console.WriteLine(person.GetBirthday());
+        }
+    }
+}
 
 public class Program
 {
     public static void Main(string[] args)
     {
-        //// Your solution should make the following code run without errors!
+        // Your solution should make the following code run without errors!
 
-        //// Step 1:
-        //// Creating instances of Student and Teacher
-        //Student studentAlice = new Student("Alice", 10, new DateTime(2007, 1, 20));
-        //Student studentEthan = new Student("Ethan", 12, new DateTime(2005, 3, 10));
-        //Teacher teacherJohnson = new Teacher("Mrs. Johnson", "Math", new DateTime(1980, 12, 10));
+        // Step 1:
+        // Creating instances of Student and Teacher
+        Student studentAlice = new Student("Alice", 10, new DateTime(2007, 1, 20));
+        Student studentEthan = new Student("Ethan", 12, new DateTime(2005, 3, 10));
+        Teacher teacherJohnson = new Teacher("Mrs. Johnson", "Math", new DateTime(1980, 12, 10));
 
-        //// Creating a list of people
-        //List<Person> people = new List<Person> { studentAlice, studentEthan, teacherJohnson };
+        // Creating a list of people
+        List<Person> people = new List<Person> { studentAlice, studentEthan, teacherJohnson };
 
-        //// Step 2:
-        //// Using dependency injection to create a School instance
-        //School school = new School(people);
+        // Step 2:
+        // Using dependency injection to create a School instance
+        School school = new School(people);
 
-        //// List all birthdays
-        //school.ListBirthdays();
+        // List all birthdays
+        school.ListBirthdays();
     }
 }
--- Mod4Week4Assessment/README.md
+++ Mod4Week4Assessment/README.md
@@ -33,27 +33,34 @@ Open up `InterfacePractice.cs`. You should not need to run this file, you will j
 Edit this file with your answers.
 
 1. What are some of the benefits of using inheritance? (1 point)
-    * < Your answer >  
+    * < Inheritance allows for cleaner, more readable code that can reuse code from the Parent class
+	without rewriting it again and again. It also allows for organization and hierarchy. It shows organized 
+	relationships between classes and between other objects, making the code easier to understand.>  
 2. What might be some of the disadvantages of using inheritance? (1 point)
-    * < Your answer >  
+    * < Inheritance can lead to Inheritance Bloat where the Parent class has too many methods and properties
+	and some of the Child Classes may inherit some that they don't need to use. Also testing can be harder with
+	inheritance since you would need to test both the behavior of the Parent class and the specific
+	behavior of the Child classes>  
 
 3. How would you describe the difference between the base class and the derived class when working with inheritance? (1 point)
-	* < Your answer >  
+	* < The Base class is the origin of the inherited properties and methods, while the derived classes are recieveing them.
+	The Base class properties and methods can also be added upon by the derived class.>  
 
 4.  What happens if you define the same method in the parent class and the child class? (1 point)
-	* < Your answer > 
+	* < This can lead to either method overriding or method hiding. If you include the override keyword the Child class will override the Parent method, but if you use the new keyword it will lead to method hiding where the Child method does not override the Parent method, but has its own method details.
+	If no keywords are used, you will get a warning about using the same name. This is called method shadowing.> 
 
 5. In your own words, how would you define an Interface? (1 point)
-    * < Your answer > 
+    * < An Interface is a set of rules or a contract that the class that implement it must abide by. They must have the properties and the methods defined by the Interface included in them. > 
 
 6. Does a class implementing an interface need to implement all of the methods in that interface? Why or why not? (1 point)
-    * < Your answer > 
+    * < Yes, the class needs to have all the methods and properties defined in the Interface to fulfill the contract. > 
 
 7. Both Inheritance and Interfaces use the `:` symbol after a class name. If you're looking at a class, what's one thing that can help you determine if the class is implementing an interface of extending a base class? (1 point)
-	* < Your answer >  
+	* < The Interfaces usually are named with IName structure and can usualy be found that way. Also in C# there is no multiple class inheritance, but there is multiple interface inheritance. If you see two objects after the ":" separated by a comma, then those are interfaces.>  
 
 8. If asked in an interview, how would you describe the purpose of the IOC container in a .NET application? (1 point)
-	* < Your answer >  
+	* < Inversion of Control (IOC) containers helps with Dependency Injection and all of the benefits that it provides. IOC containers store and manage the dependencies created in the program. >  
 
 
 ## Rubric
